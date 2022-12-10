76f59ecfd06a1c754f6b5e66e6dc5ebf947e3e9d
Ajaya <kshettryajaya@gmail.com>
2022-12-10 15:12:09 +1100

    Unions

--- Plaints/app.js
+++ Plaints/app.js
@@ -1,12 +1,24 @@
-var person;
-person = {
-    name: 'Ajaya',
-    age: 23,
-    hobbies: ['eat', 'sleep'],
-    role: [2, 'author  ']
-};
-person.role.push('Writer');
-person.hobbies.forEach(function (element) {
-    console.log(element);
-});
-console.log(person);
+//This is an union type which is separated with or operator. The values of the input can be either one of them but you will get an error when you try to perform activities related to only on type
+// For eg: If you try to change input1.toUpperCase() you will get an error becasue it is not valid on number type. so in order to perform those activities we will first check the type with if statements and then perform the operations related to the type
+function combine(input1, input2, resultConversion //This is called literal type because even though its type has not been defined but it can have only two string values which are fixed
+) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number")) {
+        result = +input1 + +input2;
+    }
+    else {
+        if (typeof input1 === "string" && typeof input2 === "string" && resultConversion === "as-number") {
+            result = parseFloat(input1) + parseFloat(input2);
+        }
+        else {
+            result = input1.toString() + input2.toString();
+        }
+    }
+    return result;
+}
+var combinedAges = combine(20, 36, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("20", "36", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("max", "Anna", "as-text");
+console.log(combinedNames);
