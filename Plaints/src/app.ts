9a65a35990881320c9e6dbd9cdfd812443fcf2a2
Ajaya <kshettryajaya@gmail.com>
2022-12-19 15:12:58 +1100

    Type gurad switch

--- Plaints/src/app.ts
+++ Plaints/src/app.ts
@@ -59,3 +59,27 @@ function useVehicle(vehicle : Vehicle){
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: 'Bird'
+  flyingSpeed: number
+}
+interface Horse {
+  type: 'Horse'
+  runningSpeed: number
+}
+type Animal = Bird | Horse
+
+function moveAnimal(animal : Animal){
+  let speed;
+  switch(animal.type){
+    case 'Bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'Horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log('Moving at speed: ' + speed);
+}
+moveAnimal({type: 'Bird', flyingSpeed: 10});
